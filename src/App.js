855e7a98e11d254a9a290b8271c7e897b1597072
Weontack Hong <aosiaos90@gmail.com>
2020-02-10 17:23:17 +0000

    event and state

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: "welcome",
+      mode: "read",
+      selected_content_id: 2,
       subject: { title: "WEB", sub: "World Wide Web!" },
       welcome: { title: "Welcome", desc: "Hello, React!" },
       contents: [
@@ -19,17 +20,40 @@ class App extends Component {
     };
   }
   render() {
+    console.log("App Render");
+    var _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+    }
     return (
       <div className="App">
         <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
+          onChangePage={function() {
+            this.setState({ mode: "welcome" });
+          }.bind(this)}
         ></Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
-        ></Content>
+        <TOC
+          onChangePage={function(id) {
+            this.setState({ mode: "read", selected_content_id: Number(id) });
+          }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
